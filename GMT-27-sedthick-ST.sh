c1b09d23810f1ef5a3f0f03b1b7b6515031e4d9b
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-07-15 21:35:14 +0300

    Add files via upload

---
+++ GMT-27-sedthick-ST.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Purpose: shaded relief grid raster map from the GEBCO dataset (here: Indian Ocean, Sunda Trench)
+# GMT modules: gmtset, gmtdefaults, grdcut, makecpt, grdimage, psscale, grdcontour, psbasemap, gmtlogo, psconvert
+
+# GMT set up
+gmt set FORMAT_GEO_MAP=dddF \
+    MAP_FRAME_PEN=dimgray \
+    MAP_FRAME_WIDTH=0.1c \
+    MAP_TITLE_OFFSET=1c \
+    MAP_ANNOT_OFFSET=0.1c \
+    MAP_TICK_PEN_PRIMARY=thinner,dimgray \
+    MAP_GRID_PEN_PRIMARY=thin,white \
+    MAP_GRID_PEN_SECONDARY=thinnest,white \
+    FONT_TITLE=12p,Palatino-Roman,black \
+    FONT_ANNOT_PRIMARY=7p,Helvetica,dimgray \
+    FONT_LABEL=7p,Helvetica,dimgray
+# Overwrite defaults of GMT
+gmtdefaults -D > .gmtdefaults
+
+# Make raster image
+grdcut GlobSed-v2.nc -R90/130/-20/10 -Gst_sed.nc
+gdalinfo st_sed.nc -stats
+# 1.635289669036865,12598
+
+# Generate a file
+ps=ST_Sediments.ps
+
+# Make color palette
+gmt makecpt -Ccyclic.cpt -V -T1/12598/250 > colors.cpt
+
+# Make raster image
+gmt grdimage st_sed.nc -Ccolors.cpt -R90/130/-20/10 -JM6i -P -I+a15+ne0.75 -Xc -K > $ps
+    
+# Add grid
+gmt psbasemap -R -J \
+    -Bpx10f5a5 -Bpyg10f5a5 -Bsxg5 -Bsyg5 \
+    --MAP_TITLE_OFFSET=0.8c \
+    --FONT_ANNOT_PRIMARY=6p,Helvetica,black \
+    --FONT_LABEL=7p,Helvetica,black \
+    -B+t"Sediment thickness around the Sunda Trench, Java and Sumatra" \
+    -Lx12.7c/-1.3c+c318/-57+w800k+l"Mercator projection. Scale: km"+f \
+    -UBL/-5p/-40p -O -K >> $ps
+
+# Add legend
+gmt psscale -Dg84.0/-20+w11.4c/0.4c+v+o0.3/0i+ml -R -J -Ccolors.cpt \
+    --FONT_LABEL=6p,Helvetica,dimgray \
+    --FONT_ANNOT_PRIMARY=6p,Helvetica,black \
+    -Ba1000f500+l"Color scale 'cyclic': Cyclic colormap, spans 360 degrees of hue [C=HSV] 1/12598)" \
+    -I0.2 -By+lm -O -K >> $ps
+    
+# Add isolines
+gmt grdcontour st_sed.nc -R -J -C1000 -Wthinnest,white -O -K >> $ps
+
+# Add coastlines, borders, rivers
+gmt pscoast -R -J -P -Ia/thinnest,blue -Na -N1/thinner,red -Wthinner -Df -O -K >> $ps
+
+# Add GMT logo
+gmt logo -Dx6.2/-2.0+o0.1i/0.1i+w2c -O -K >> $ps
+
+# Add subtitle
+gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y5.7c -N -O \
+    -F+f10p,Palatino-Roman,black+jLB >> $ps << EOF
+4.5 10.0 GlobSed 5 arc min grid V-3
+EOF
+
+# Convert to image file using GhostScript
+gmt psconvert ST_Sediments.ps -A0.5c -E720 -Tj -Z
