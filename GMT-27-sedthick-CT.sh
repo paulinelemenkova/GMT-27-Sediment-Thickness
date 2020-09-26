09fc04fa14b3434cfd9247d5327892ad7cd68684
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-09-26 21:53:02 +0300

    Add files via upload

--- GMT-27-sedthick-CT.sh
+++ GMT-27-sedthick-CT.sh
@@ -4,16 +4,16 @@
 
 # GMT set up
 gmt set FORMAT_GEO_MAP=dddF \
-MAP_FRAME_PEN=dimgray \
+MAP_FRAME_PEN=black \
 MAP_FRAME_WIDTH=0.1c \
 MAP_TITLE_OFFSET=1c \
 MAP_ANNOT_OFFSET=0.1c \
-MAP_TICK_PEN_PRIMARY=thinner,dimgray \
+MAP_TICK_PEN_PRIMARY=thinner,black \
 MAP_GRID_PEN_PRIMARY=thin,white \
 MAP_GRID_PEN_SECONDARY=thinnest,white \
-FONT_TITLE=12p,Palatino-Roman,black \
-FONT_ANNOT_PRIMARY=7p,Helvetica,dimgray \
-FONT_LABEL=7p,Helvetica,dimgray \
+FONT_TITLE=12p,Helvetica,black \
+FONT_ANNOT_PRIMARY=7p,Helvetica,black \
+FONT_LABEL=7p,Helvetica,black \
 
 # Make raster image
 grdcut GlobSed-v2.nc -R224/240/35/55 -Gct_sed.nc
@@ -33,15 +33,15 @@ gmt grdimage ct_sed.nc -Ccolors.cpt -R224/240/35/55 -JM6i -P -I+a15+ne0.75 -Xc -
 gmt psbasemap -R -J \
     -Bpxg8f2a4 -Bpyg6f2a2 -Bsxg4 -Bsyg2 \
     --MAP_TITLE_OFFSET=1.0c \
-    --FONT_ANNOT_PRIMARY=8p,Helvetica,dimgray \
+    --FONT_ANNOT_PRIMARY=8p,Helvetica,black \
     --MAP_ANNOT_OFFSET=0.1c \
     -B+t"Sediment thickness around Cascadia Trench seafloor, west Canada" -O -K >> $ps
     
 # Add legend
 gmt psscale -Dg217/35+w15.0c/0.4c+h+o7.0/-1.5c+ml -R -J -Ccolors.cpt \
-    --FONT_LABEL=8p,Helvetica,dimgray \
-    --FONT_ANNOT_PRIMARY=5p,Helvetica,dimgray \
-    -Baf+l"Color scale: turbo (Google's Improved Rainbow Colormap for Visualization [0/2339, C=RGB])" \
+    --FONT_LABEL=8p,Helvetica,black \
+    --FONT_ANNOT_PRIMARY=8p,Helvetica,black \
+    -Bg200f40a200+l"Color scale: turbo (Google's Improved Rainbow Colormap for Visualization [0/2339, C=RGB])" \
     -I0.2 -By+lm -O -K >> $ps
     
 # Add shorelines
@@ -49,19 +49,18 @@ gmt grdcontour ct_sed.nc -R -J -C100 -A300 -Wthinnest,white -O -K >> $ps
 
 # Add scale, directional rose
 gmt psbasemap -R -J \
-    --FONT=8p,Palatino-Roman,dimgray \
+    --FONT=8p,Helvetica,black \
     --MAP_TITLE_OFFSET=0.3c \
-    -Lx13.4c/-2.7c+c50+w300k+l"Mercator projection. Scale (km)"+f \
+    -Lx13.4c/-2.8c+c50+w300k+l"Mercator projection. Scale (km)"+f \
     -UBL/0p/-80p -O -K >> $ps
     
 # Add GMT logo
 gmt logo -Dx6.4/-3.5+o0.1i/0.1i+w2c -O -K >> $ps
 
-
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/14 -X0.5c -Y7.1c -N -O \
-    -F+f10p,Palatino-Roman,black+jLB >> $ps << EOF
-5.0 22.5 GlobSed 5 arc min grid V-3
+    -F+f10p,Helvetica,black+jLB >> $ps << EOF
+5.0 22.7 GlobSed 5 arc min grid V-3
 EOF
 
 # Convert to image file using GhostScript
